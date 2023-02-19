2faaf86fd4ece06da77f2db5fb4e2e4e6ddb1337
Ponczo13 <114801388+Ponczo13@users.noreply.github.com>
2023-02-19 09:00:18 +0100

    Add files via upload

---
+++ Potegi.java
@@ -0,0 +1,10 @@
+public class Potegi {
+    public static void main(String[] args) {
+        int podstawa = 2;
+        int potega = 1;
+        for (int i=0;i<=10;i++){
+            System.out.println(potega);
+            potega=potega*podstawa;
+        }
+    }
+}
